824bf4b1a43fd1926fcbc04c620bb4c1853800ac
Svyatoslav <slava.perchuk@gmail.com>
2017-04-17 14:59:09 +0300

    Update creating_tables.sql

--- creating_tables.sql
+++ creating_tables.sql
@@ -1,19 +1,20 @@
 CREATE TABLE users (
   user_id INT PRIMARY KEY ,
-  user_name text ,
+  user_name text NOT NULL,
   birth_date date ,
   password text
 );
 
 CREATE TABLE conversations (
   conversation_id INT PRIMARY KEY ,
-  conversation_name text ,
+  conversation_name text UNIQUE NOT NULL,
   creator_id INT
 );
 
 CREATE TABLE user_conversation (
-  user_id INTEGER REFERENCES users (user_id) ,
-  coversation_id INTEGER REFERENCES conversations (conversation_id)
+  user_id INT REFERENCES users (user_id) NOT NULL,
+  coversation_id INT REFERENCES conversations (conversation_id) NOT NULL,
+  UNIQUE (user_id, conversation_id)
 );
 
 CREATE TABLE messages (
@@ -24,13 +25,14 @@ CREATE TABLE messages (
 );
 
 CREATE TABLE conversation_message (
-  message_id INT REFERENCES messages (message_id) ,
-  conversation_id INT REFERENCES conversations (conversation_id)
+  message_id INT REFERENCES messages (message_id) NOT NULL,
+  conversation_id INT REFERENCES conversations (conversation_id) NOT NULL,
+  UNIQUE (message_id, conversation_id)
 );
 
 CREATE TABLE attachments (
   attachment_id INT PRIMARY KEY ,
-  message_id INT REFERENCES messages (message_id),
+  message_id INT REFERENCES messages (message_id) UNIQUE NOT NULL,
   file UUID--some tipe of data
 );
 
